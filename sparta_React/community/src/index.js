fdb9545f261af4049286bc3e83cfe15a24504ad9
skdud4659 <skdud4659@gmail.com>
2021-07-05 09:49:49 +0900

    community

--- sparta_React/community/src/index.js
+++ sparta_React/community/src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './shared/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from "react-redux";
-
-import store from "./redux/configureStore";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
